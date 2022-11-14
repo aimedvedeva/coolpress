2c08a9450c23670b8c1e59ceb2e4ffc0ad7efb37
Aleksandra Medvedeva <alesan.med@yandex.ru>
2022-11-14 23:48:06 +0400

    add some templates and implement create post button in navbar

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -3,7 +3,10 @@ from press import views
 
 urlpatterns = [
     path('authors/', views.authors),
-    path('home/', views.home),
+    path('home/', views.home, name='home'),
     path('posts/', views.posts_list, name='posts-list'),
     path('post_details/<int:post_id>', views.post_detail, name='posts-detail'),
+    path('post/<int:post_id>/comment-add/', views.add_post_comment, name='comment-add'),
+    path('post/update/<int:post_id>', views.post_update, name='post-update'),
+    path('post/add/', views.post_create, name='post-create'),
 ]
--- coolpress/press/views.py
+++ coolpress/press/views.py
@@ -1,10 +1,10 @@
 import datetime
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-from press.models import Category, Post, CoolUser
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from press.forms import CommentForm, PostForm
+from press.models import Category, Post, Comment, CoolUser
 from django.db.models import Count
 
 def home(request):
@@ -23,7 +23,7 @@ def home(request):
     return HttpResponse(html)
 
 def authors(request):
-    authors = CoolUser.objects.all().values('user__first_name', 'user__last_name', 'user__username', 'github_profile','gh_repositories')
+    authors = CoolUser.objects.all().values('user__first_name', 'user__last_name', 'user__username', 'github_profile', 'gh_repositories')
     li_authors = [f'<li><b>First name:</b>{author["user__first_name"]} <b>Last name:</b>{author["user__last_name"]} <b>Username:</b> {author["user__username"]} <b>Github profile:</b> {author["github_profile"]} <b>Repositories:</b> {author["gh_repositories"]}</li>' for author in authors]
     authors_ul = f'<ul>{"".join(li_authors)}</ul>'
 
@@ -41,4 +41,60 @@ def posts_list(request):
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
-    return render(request, 'posts_detail.html', {'post_obj': post})
+    data = request.POST or {'votes': 10}
+    form = CommentForm(data)
+
+    comments = post.comment_set.order_by('-creation_date')
+    return render(request, 'posts_detail.html', {'post_obj': post, 'comment_form': form, 'comments': comments})
+
+@login_required
+def add_post_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    data = request.POST or {'votes': 10}
+    form = CommentForm(data)
+    if request.method == 'POST':
+        if form.is_valid():
+            votes = form.cleaned_data.get('votes')
+            body = form.cleaned_data['body']
+            Comment.objects.create(votes=votes, body=body, post=post, author=request.user.cooluser)
+            return HttpResponseRedirect(reverse('posts-detail', kwargs={'post_id': post_id}))
+
+    return render(request, 'comment-add.html', {'form': form, 'post': post})
+
+@login_required
+def post_update(request, post_id=None):
+    post = None
+    if post_id:
+        post = get_object_or_404(Post, pk=post_id)
+        if request.user != post.author.user:
+            return HttpResponseBadRequest('Not allowed to change others posts')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            username = request.user.username
+            instance.author = CoolUser.objects.get(user__username=username)
+            instance.save()
+            return HttpResponseRedirect(reverse('posts-detail', kwargs={'post_id': instance.id}))
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'posts_update.html', {'form': form})
+
+@login_required
+def post_create(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            body = form.cleaned_data['body']
+            image_link = form.cleaned_data.get('image_link')
+            category = form.cleaned_data.get('category')
+            status = form.cleaned_data.get('status')
+            post = Post.objects.create(title=title, body=body, image_link=image_link, category=category, status=status,
+                                   author=request.user.cooluser)
+            post.save()
+            return HttpResponseRedirect(reverse('home'))
+    return render(request, 'posts_create.html', {'form': form})
