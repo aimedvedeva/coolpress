038a11c124e03efca62242a11c43391bcb025b8c
Aleksandra Medvedeva <alesan.med@yandex.ru>
2022-11-15 14:26:10 +0400

    add superform basen on CBV

--- coolpress/press/views.py
+++ coolpress/press/views.py
@@ -1,11 +1,16 @@
 import datetime
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.db.models import Count
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views.generic import TemplateView, ListView
+from rest_framework import viewsets, mixins, permissions
+from rest_framework.viewsets import GenericViewSet
 from press.forms import CommentForm, PostForm, CategoryForm
-from press.models import Category, Post, Comment, CoolUser
 from django.db.models import Count
+from press.models import Category, Post, Comment, CoolUser, PostStatus
+from press.serializers import CategorySerializer, PostSerializer, AuthorSerializer
 
 def home(request):
     now = datetime.datetime.now()
@@ -80,7 +85,7 @@ def post_update(request, post_id=None):
     else:
         form = PostForm(instance=post)
 
-    return render(request, 'posts_update.html', {'form': form})
+    return render(request, 'posts_update.html', {'my_awesome_form': form})
 
 @login_required
 def post_create(request):
@@ -111,4 +116,103 @@ def category_create(request):
             category.save()
             return HttpResponseRedirect(reverse('home'))
     return render(request, 'categories_create.html', {'form': form})
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+class CategoryListView(ListView):
+    model = Category
+
+
+class PostClassBasedListView(ListView):
+    paginate_by = 20
+    queryset = Post.objects.filter(status=PostStatus.PUBLISHED).order_by('-last_update')
+    context_object_name = 'posts_list'
+    template_name = 'posts_list.html'
+
+
+class PostClassFilteringListView(PostClassBasedListView):
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super(PostClassFilteringListView, self).get_queryset()
+        category = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        return queryset.filter(category=category)
+
+
+def category_api(request, slug):
+    cat = get_object_or_404(Category, slug=slug)
+    return JsonResponse(
+        dict(slug=cat.slug, label=cat.label)
+    )
+
+
+def categories_api(request):
+    cats = {}
+    for cat in Category.objects.all():
+        cats[cat.id] = dict(slug=cat.slug, label=cat.label)
+
+    return JsonResponse(
+        cats
+    )
+
+
+class ModelNonDeletableViewSet(mixins.CreateModelMixin,
+                               mixins.RetrieveModelMixin,
+                               mixins.UpdateModelMixin,
+                               # mixins.DestroyModelMixin,
+                               mixins.ListModelMixin,
+                               GenericViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    pass
+
+
+class CategoryViewSet(ModelNonDeletableViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.author == request.user.cooluser
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Post.objects.all().filter(status=PostStatus.PUBLISHED) \
+        .order_by('-creation_date')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.cooluser)
+
+class AuthorsViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = CoolUser.objects.alias(posts=Count('post')).filter(posts__gte=1)
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
